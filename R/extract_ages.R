19ddc14e2507f500d5e06a71b29bb83fec088908
millacarmona <pablomillac@gmail.com>
2022-04-18 20:19:08 -0300

    correcting path specification in extract_ages

--- R/extract_ages.R
+++ R/extract_ages.R
@@ -52,7 +52,11 @@ extract_ages <- function(path = NA, type, sample) {
   #loop through the tree files, load them and subsample them to the number
   #specified in 'sample'
   for(i in 1:length(files)) {
-    trees <- ape::read.tree(paste0(path, '/', files[i]))
+    if(is.na(path)) {
+      trees <- ape::read.tree(paste0(getwd(), '/', files[i]))
+    } else {
+        trees <- ape::read.tree(paste0(path, '/', files[i]))
+        }
     if(!is.na(sample)) {
       trees <- trees[sample(1:length(trees), sample)]
     }
