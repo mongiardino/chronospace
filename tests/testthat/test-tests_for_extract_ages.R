d0080797a2c56d9a77ced2d212eb76f866b99b73
millacarmona <pablomillac@gmail.com>
2024-02-07 16:46:49 -0300

    updating / adding tests for graphical outputs and extract_ages

--- tests/testthat/test-tests_for_extract_ages.R
+++ tests/testthat/test-tests_for_extract_ages.R
@@ -3,14 +3,14 @@ test_that(desc = "tests for extract_ages", code = {
   dir.create(paste(temp0, "files", sep="\\"))
   temp <- paste(temp0, "files", sep="\\")
 
-  url <- "https://raw.githubusercontent.com/mongiardino/chronospace/main/example_files/"
+  url <- "https://raw.githubusercontent.com/mongiardino/chronospaces_eLife/main/example_files/"
   files <- c("clockCATGTR_ln_sample.datedist",
              "clockGTR_ln_sample.datedist",
              "randomCATGTR_ln_sample.datedist",
              "randomGTR_ln_sample.datedist",
              "signalCATGTR_ln_sample.datedist",
              "signalGTR_ln_sample.datedist")
-  for(i in 1:length(files)) utils::download.file(paste0(url, files[i]), paste(temp, files[i], sep="\\"))
+  for(i in 1:length(files)) download.file(paste0(url, files[i]), paste(temp, files[i], sep = "/"))
 
   type <- list(c('clock', 'clock', 'random', 'random', 'signal', 'signal'),
                c('CATGTR', 'GTR', 'CATGTR', 'GTR', 'CATGTR', 'GTR'))
@@ -18,11 +18,15 @@ test_that(desc = "tests for extract_ages", code = {
 
   data <- extract_ages(path = temp, type = type, sample = sample)
 
-  expect_match(class(data), "dataAges", all = FALSE)
+  expect_match(class(data), "nodeAges", all = FALSE)
   expect_match(class(data), "list", all = FALSE)
+
+  expect_true(all(names(data) == c("ages", "factors", "topology")))
   expect_equal(data$topology$Nnode, ncol(data$ages))
+  expect_equal(nrow(data$ages), nrow(data$factors))
 
 })
+
 
 
 
--- tests/testthat/test-tests_for_graphical_outputs.R
+++ tests/testthat/test-tests_for_graphical_outputs.R
@@ -1,13 +1,13 @@
 test_that(desc = "tests for plot.chronospace", code = {
   data("echinoid_dates")
   cspace <- chronospace(data_ages = echinoid_dates)
-  ordination <- plot(obj=cspace)
+  ordination <- plot.chronospace(obj=cspace, output = "none")
 
   expect_match(class(ordination), "list")
-  expect_equal(length(ordination), length(cspace))
+  expect_equal(length(ordination), ncol(echinoid_dates$factors))
   for(i in 1:length(ordination)) expect_equal(names(ordination[[i]]), c("ordination", "PC_extremes"))
   for(i in 1:length(ordination)) expect_match(class(ordination[[i]]$ordination), "ggplot", all = FALSE)
-  for(i in 1:length(ordination)) expect_equal(length(ordination[[i]]$PC_extremes), ncol(cspace[[i]]$x))
+  for(i in 1:length(ordination)) expect_equal(length(ordination[[i]]$PC_extremes), ncol(cspace[[i]]$ordination$x))
   for(i in 1:length(ordination)) {
     ord_i <- ordination[[i]]
     for(j in 1:length(ord_i$PC_extremes)) expect_match(class(ord_i$PC_extremes[[j]]), "ggtree", all = FALSE)
@@ -18,10 +18,9 @@ test_that(desc = "tests for plot.chronospace", code = {
 
 test_that(desc = "tests for sensitive_nodes", code = {
   data("echinoid_dates")
-  cspace <- chronospace(data_ages = echinoid_dates)
-  sensinodes <-sensitive_nodes(obj = cspace, chosen_clades = 5)
+  sensinodes <- sensitive_nodes(echinoid_dates, num_clades = 5, plot = FALSE)
 
-  expect_equal(length(sensinodes), length(cspace))
+  expect_equal(length(sensinodes), ncol(echinoid_dates$factors))
   for(i in 1:length(sensinodes)) expect_match(class(sensinodes[[i]]), "ggarrange", all = FALSE)
 
 })
@@ -29,9 +28,22 @@ test_that(desc = "tests for sensitive_nodes", code = {
 
 test_that(desc = "tests for ltt_sensitivity", code = {
   data("echinoid_dates")
-  sensiltt<-ltt_sensitivity(data_ages = echinoid_dates, average = "mean")
+  sensiltt <- ltt_sensitivity(echinoid_dates, plot = FALSE)
 
   expect_equal(length(sensiltt), ncol(echinoid_dates$factors))
   for(i in 1:length(sensiltt)) expect_match(class(sensiltt[[i]]), "ggplot", all = FALSE)
 
 })
+
+
+test_that(desc = "tests for ", code = {
+  data("echinoid_dates")
+  specnode <- specified_node(echinoid_dates, tips = c('Brissus_obesus',
+                                                      'Abatus_cordatus'), plot = TRUE)
+
+  expect_equal(length(specnode), ncol(echinoid_dates$factors))
+  for(i in 1:length(specnode)) expect_match(class(specnode[[i]]), "ggplot", all = FALSE)
+
+})
+
+
